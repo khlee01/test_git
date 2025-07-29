4009126a92928fa99619310d501334f3786c0f43
siyun <khleebluefly11@gmail.com>
2025-07-29 12:27:26 +0900

    third

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import asyncio
+import requests
+from requests.auth import HTTPDigestAuth
+
 from playwright.async_api import async_playwright
 
 async def run():
@@ -8,6 +11,17 @@ async def run():
         await page.goto("https://example.com")
         title = await page.title()
         print(f"Page title: {title}")
+
+    url = "https://httpbin.org/digest-auth/auth/user/passwd"
+    response = requests.get(url, auth=HTTPDigestAuth('user', 'passwd'))
+
+    if response.status_code == 200:
+        print("인증 성공!")
+        print(response.json())
+    else:
+        print(f"인증 실패: {response.status_code}")
+
+
         await browser.close()
 
 # 실행
