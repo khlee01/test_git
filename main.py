31a621dc105d1c0d4376240ef880d8cafff92a01
siyun <khleebluefly11@gmail.com>
2025-07-29 11:33:06 +0900

    second commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio
+from playwright.async_api import async_playwright
+
+async def run():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=False)  # headless=False 하면 브라우저 보임
+        page = await browser.new_page()
+        await page.goto("https://example.com")
+        title = await page.title()
+        print(f"Page title: {title}")
+        await browser.close()
+
+# 실행
+asyncio.run(run())
